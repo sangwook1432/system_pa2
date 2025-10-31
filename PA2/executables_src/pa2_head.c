13d901438c2ad4118284c3e967f3152f72d99309
sangwook1432 <103094090+sangwook1432@users.noreply.github.com>
2025-10-31 11:58:36 +0900

    Fix endptr check in number parsing

--- PA2/executables_src/pa2_head.c
+++ PA2/executables_src/pa2_head.c
@@ -38,7 +38,7 @@ int main(int argc,char *argv[]) {
         char *endptr;
         errno = 0;
         long num = strtol(optarg,&endptr,10);
-        if(errno != 0 || endptr != '\0' || endptr == optarg || num < 0){
+        if(errno != 0 || *endptr != '\0' || endptr == optarg || num < 0){
           fprintf(stderr,"pa2_head: invalid number of lines: '%s'\n",optarg);
           return EXIT_FAILURE;
         }
